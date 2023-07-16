228d193d7a11d88b4d4b592c6c26ce9288da364a
Dagadev <gacub20ru@gmail.com>
2023-07-15 19:27:50 -0600

    add db by mongodb

---
+++ src/utils/db.ts
@@ -0,0 +1,17 @@
+import { connect, set } from "mongoose";
+
+set('strictQuery', true)
+let isConnected = false
+
+export async function dbConnect() {
+  try {
+    if(isConnected) return
+
+    await connect(process.env.CONNECT_MONGO || '')
+    isConnected = true
+    console.log('✅ Connected to database')
+
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+  }
+}
