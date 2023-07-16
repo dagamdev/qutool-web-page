d915e43e4dfbfc7240e89a257cf54b256cc7d21e
Dagadev <gacub20ru@gmail.com>
2023-07-15 19:27:17 -0600

    remove api endpint

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,3 @@
-// export const END_POINT = 'http://localhost:246/api/v1/Qutool/'
-export const END_POINT = 'https://my-api-production-065b.up.railway.app/api/v1/Qutool/'
-
 export const DISCORD_END_POINT = 'https://discord.com/api/v10/'
 export const QUTOOL_INVITE_URL = 'https://discord.com/api/oauth2/authorize?client_id=935707268090056734&permissions=1374725467383&scope=bot%20applications.commands'
 export const PROTECTED_ROUTES: readonly string[] = ['/dashboard']
-
