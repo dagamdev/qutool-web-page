73de704c25bae6700af4f7f6f57f3e0e49be57ef
Dagadev <gacub20ru@gmail.com>
2023-07-16 22:09:38 -0600

    add consent dialog

--- src/components/shared/index.tsx
+++ src/components/shared/index.tsx
@@ -1,18 +1,21 @@
 'use client'
 
-import { useTooltips } from "@/hooks"
+import { useDialog, useTooltips } from "@/hooks"
 import Tooltip from "./tooltip"
 import Header from "./header"
 import CursorTarget from "./cursorTarget"
+import ConsenNotice from "../dialog/ConsentNotice"
 
 export default function SharedComponents(){
   const { tooltips } = useTooltips()
+  const { showDialog } = useDialog()
   
   return (
     <>
       <Header />
       <CursorTarget />
       {tooltips && tooltips.map(t=> <Tooltip key={t.id} tooltip={t} />)}
+      {showDialog && <ConsenNotice />}
     </>
   )
 }
