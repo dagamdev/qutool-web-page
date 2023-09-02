ed77a09de157c775a53f88b64f90e6588a1513dc
Dagadev <gacub20ru@gmail.com>
2023-09-01 21:50:53 -0600

    update login button

--- src/components/button/LogInButton.tsx
+++ src/components/button/LogInButton.tsx
@@ -3,11 +3,13 @@
 import styles from './button.module.scss'
 import Link from 'next/link'
 import { useLanguage, useDialog, useUser } from '@/hooks'
+import { getCSRLanguage } from '@/utils/services'
 
 export default function LogInButton(){
   const { textLang } = useLanguage()
   const { user } = useUser()
   const { openDialog } = useDialog()
+  const isEnglish = getCSRLanguage() == 'en'
 
   const handleClick = () => {
     openDialog()
@@ -16,7 +18,7 @@ export default function LogInButton(){
   return (
     <>
       {user ? 
-        <Link href={'/dashboard'} className={`${styles.button} button`} >{textLang.dashboard}</Link> :  
+        <Link href={'/servers'} className={`${styles.button} button`} >{textLang.manageServer}{isEnglish ? 's' : 'es'}</Link> :  
         <button onClick={handleClick} className={styles.button}>{textLang.logIn}</button>
       }
     </>
