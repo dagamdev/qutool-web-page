7e76afffe612f58a459604afa748011b6c59dcb9
Dagadev <gacub20ru@gmail.com>
2023-09-02 20:08:11 -0600

    update dialog

--- src/components/dialog/ConsentNotice.tsx
+++ src/components/dialog/ConsentNotice.tsx
@@ -2,10 +2,9 @@
 
 import styles from './dialog.module.scss'
 import Link from 'next/link'
-import { useRef, useState, useEffect, type MouseEvent } from 'react'
+import { useRef, useState, useEffect, type MouseEvent, type AnimationEvent } from 'react'
 import { useDialog, useLanguage } from "@/hooks"
 import { transformText, windowExist } from "@/utils/services"
-import texts from '@/utils/texts.json'
 
 const CONCENT_CONTENT = {
   en: (<>
@@ -19,37 +18,38 @@ const CONCENT_CONTENT = {
 export default function ConsenNotice(){
   const { lang } = useLanguage()
   const { closeDialog } = useDialog()
-  const [thisRef, setThisRef] = useState<HTMLDivElement | null>(null)
+  const { textLang } = useLanguage()
+  const [close, setClose] = useState(false)
   const cardRef = useRef<HTMLDivElement>(null)
-
-  // useEffect(()=> {
-  //   if(thisRef){
-
-  //   }
-  // }, [thisRef])
 
   const handleClick = (event: MouseEvent<HTMLDivElement>) => {
     if(event.target instanceof Node){
-      if(!cardRef.current?.contains(event.target)) closeDialog()
-      
+      if(!cardRef.current?.contains(event.target)) setClose(true)
     }
   }
 
   const signIn = () => {
     if(windowExist) window.location.href = 'api/auth'
-    closeDialog()
+    setClose(true)
   }
 
+  const handleAnimationEnd = ({target}: AnimationEvent<HTMLDivElement>) => {
+    console.log(target)
+    if(close && target instanceof HTMLElement && target.classList.contains(styles.hide)) {
+      console.log('close')
+      closeDialog()
+    }
+  }
   
   return (
-    <div onClick={handleClick} className={styles.dialog}>
+    <div onClick={handleClick} className={`${styles.dialog} ${close ? styles.hide : ''}`} onAnimationEnd={handleAnimationEnd}>
       <div ref={cardRef} className={styles['dialog-card']} >
         {/* <p dangerouslySetInnerHTML={{__html: transformText(texts[lang])}} /> */}
         <p>{CONCENT_CONTENT[lang]}</p>
 
         <div className={styles['dialog-buttons']}>
-          <button onClick={closeDialog}>In disagreement</button>
-          <button onClick={signIn}>OK</button>  
+          <button onClick={closeDialog}>{textLang.inDisagreement}</button>
+          <button onClick={signIn}>{textLang.ok}</button>  
         </div>
       </div>
     </div>
