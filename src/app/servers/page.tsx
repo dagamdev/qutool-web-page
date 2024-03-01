e33d7453c9fe2df2a7826827cfa1552ed6c65f7c
Dagadev <gacub20ru@gmail.com>
2024-02-29 19:02:17 -0600

    Update server styles

--- src/app/servers/page.tsx
+++ src/app/servers/page.tsx
@@ -5,18 +5,14 @@ import { discordFetch, getSSRTextByLang, getSession } from '@/lib'
 export default async function ServersPage(){
   const text = getSSRTextByLang()
   const session = await getSession()
-  const guilds = await discordFetch('users/@me/guilds?with_counts=true', {
-    headers: {
-      'Authorization': `Bearer ${session?.accessToken}`
-    }
-  })
+  const guilds = await discordFetch('users/@me/guilds?with_counts=true', session?.accessToken)
 
   return (
     <main className={styles.servers}>
       <h2>{text.servers}</h2>
 
-      {guilds
-        ? <Guilds guilds={guilds} />
+      {Array.isArray(guilds)
+        ? <Guilds guilds={guilds.filter(g => (+g.permissions & 0x0000000000000008) === 0x0000000000000008)} />
         : <p>Error</p>
       }
     </main>
