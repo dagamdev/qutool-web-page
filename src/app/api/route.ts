0802c29e731363c2c2f035d233ba9e99289d5e4a
Dagadev <gacub20ru@gmail.com>
2024-02-29 19:53:28 -0600

    Fix endpoint routes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  res.json({message: 'Hello'})  
+export async function GET() {
+  return new Response(JSON.stringify({message: 'Hello'})  )
 }
