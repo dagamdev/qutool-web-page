501ee85d71f34031c898062d945150b37f7e5cba
Dagadev <gacub20ru@gmail.com>
2024-02-29 18:19:42 -0600

    Add discordFetch

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,17 +1,15 @@
-import { ClientModel } from '@/models'
-import { NextApiRequest } from 'next'
+import { SessionModel } from '@/models'
+import { cookies } from 'next/headers'
 import { headers } from 'next/headers'
 import texts from '@/utils/texts.json'
-import type { Languages, Session } from '@/utils/types'
-
-export { isAuthenticate } from './middlewares'
+import type { Languages } from '@/types'
 import { getTextsLang } from './controllers'
 import { DISCORD_END_POINT } from '@/utils/config'
 export const  controllers = { getTextsLang } 
 
-export async function getClient(req: NextApiRequest): Promise<Session | null> {
-  console.log(req.session.user)
-  return await ClientModel.findOne({userId: req.session.user?.id})
+export async function getSession() {
+  const cookieStore = cookies()
+  return await SessionModel.findById(cookieStore.get('sessionId')?.value)
 }
 
 export async function getMessage(channelId: string, messageId: string): Promise<{content: string}> {
@@ -25,13 +23,26 @@ export async function getMessage(channelId: string, messageId: string): Promise<
 export function getSSRLanguage(): Languages {
   const headersList = headers()
   const languages = headersList.get('accept-language')
-  if(languages) return languages.split('-')[0] as (Languages | undefined) || 'en'
+  if(languages) return languages.split('-')[0] as (Languages | undefined) ?? 'en'
   else return 'en'
   
 }
 
 export function getSSRTextByLang() {
   return texts[getSSRLanguage()]
+}
+
+export async function discordFetch (path: string, init?: RequestInit) {
+  const response = await fetch('https://discord.com/api/v10/' + path, init)
+  const contentType = response.headers.get('content-type')
+
+  if (contentType !== 'application/json') {
+    return {
+      message: 'The response is not json'
+    }
+  }
+
+  return await response.json()
 }
 
 export async function customBotFetch(path: string) {
