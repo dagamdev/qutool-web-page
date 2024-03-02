758a5e224097125213f258318319f4c1eb6e1c04
Dagadev <gacub20ru@gmail.com>
2024-03-02 16:49:28 -0600

    Update privacy page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -3,21 +3,25 @@ import '@/styles/docs.css'
 import { transformText } from '@/utils/services'
 import { getSSRLanguage, getSSRTextByLang, controllers } from '@/lib'
 import docs from '@/utils/documents.json'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Privacy'
+}
 
 export default async function PrivacyPage(){
   const lang = getSSRLanguage()
   const text = getSSRTextByLang()
-  const privacyPolicies = await controllers.getTextsLang('PP')  
+  const privacyPolicies = await controllers.getTextsLang('PP', lang)  
 
   return (
     <main className='doc'>
-      <section id='bot' className='doc-section'>
+      <section className='doc-section'>
         <h2>{text.bot.PP}</h2>
-        <p dangerouslySetInnerHTML={{__html: transformText(privacyPolicies[lang])}}></p>
+        <p dangerouslySetInnerHTML={{__html: transformText(privacyPolicies)}}></p>
       </section>
 
-      <section id='web' className='doc-section'>
+      <section className='doc-section'>
         <h2>Políticas de privacidad del la Página</h2>
         <p dangerouslySetInnerHTML={{__html: transformText(docs[lang].PP)}} />
       </section>
