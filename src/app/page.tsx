5462418b1ed7eec9189298236a0b3ecce8b7e97e
Dagadev <gacub20ru@gmail.com>
2024-03-02 17:08:53 -0600

    Update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ import { getSSRLanguage, getSSRTextByLang, controllers } from '@/lib'
 export default async function Home() {
   const lang = getSSRLanguage()
   const text = getSSRTextByLang()
-  const description = await controllers.getTextsLang('description')
+  const description = await controllers.getTextsLang('description', lang)
 
   return (
     <main className={styles.home}>
       <h2>{text.title}</h2>
-      <p dangerouslySetInnerHTML={{__html: transformText(description[lang] || '')}}></p>
+      <p className={styles.paragraph} dangerouslySetInnerHTML={{__html: transformText(description)}}></p>
       <LogInOrDashboard />
     </main>
   )
