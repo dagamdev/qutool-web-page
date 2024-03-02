3ff290a73fc4770e7f3c08ffcf5016f11885b0bc
Dagadev <gacub20ru@gmail.com>
2024-03-02 16:49:06 -0600

    Update terms page

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,26 +1,24 @@
 import '@/styles/docs.css'
 
+import { Metadata } from 'next'
 import { getSSRLanguage, getSSRTextByLang, controllers } from '@/lib'
 import { transformText } from '@/utils/services'
+
+export const metadata: Metadata = {
+  title: 'TOS'
+}
 
 export default async function TermsPage(){
   const lang = getSSRLanguage()
   const text = getSSRTextByLang()
-  const termsOfUse = await controllers.getTextsLang('TOS')
+  const termsOfUse = await controllers.getTextsLang('TOS', lang)
   
-  console.log({termsOfUse})
-
   return (
     <main className='doc'>
-      <section id='bot' className='doc-section'>
+      <section className='doc-section'>
         <h2>{text.bot.TOS}</h2>
-        <p dangerouslySetInnerHTML={{__html: transformText(termsOfUse[lang] || '')}}></p>
+        <p dangerouslySetInnerHTML={{__html: transformText(termsOfUse)}}></p>
       </section>
-
-      {/* <section id='web' className='doc-section'>
-        <h2>Términos de Uso de la Página</h2>
-        <p>Working</p>
-      </section> */}
     </main>
   )
 }
