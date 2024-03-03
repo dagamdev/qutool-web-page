ea4792a74d66ff830f4fd58e1ccb713c68d2e07c
Dagadev <gacub20ru@gmail.com>
2024-03-03 17:19:43 -0600

    Use load dialog

--- src/components/shared/index.tsx
+++ src/components/shared/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useDialog, useTooltips } from "@/hooks"
-import Tooltip from "./tooltip"
-import Header from "./header"
-import CursorTarget from "./cursorTarget"
-import ConsenNotice from "../dialog/ConsentNotice"
+import { useDialog, useTooltips } from '@/hooks'
+import Tooltip from './tooltip'
+import Header from './header'
+import CursorTarget from './cursorTarget'
+import ConsenNotice from '../dialog/ConsentNotice'
 
 export default function SharedComponents(){
   const { tooltips } = useTooltips()
